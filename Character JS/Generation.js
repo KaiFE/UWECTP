dafcc5ce1b39e49340c9e3a8f08d92316fc6a35d
KaiFE <kaiitaly@hotmail.com>
2017-03-20 11:51:51 +0000

    Character JS

---
+++ Character JS/Generation.js
@@ -0,0 +1,9 @@
+﻿var charobj : GameObject;
+
+function Start()  {
+	yield WaitForSeconds (1.5); //duration of wait
+	var position: Vector3 = Vector3(Random.Range(0,0), Random.Range(0,4), Random.Range(0,5)); // the position of which the character can spawn
+	Instantiate(charobj, position, Quaternion.identity); // clone the objects
+	yield WaitForSeconds (0); 
+	//Destroy(gameObject);
+}
