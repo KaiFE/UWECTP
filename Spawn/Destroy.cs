56d02baa517200de1f8fafe5833c07c62151ac84
KaiFE <kaiitaly@hotmail.com>
2017-03-20 11:49:16 +0000

    Random Item Spawn

---
+++ Spawn/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	public float destroyTime = 3.0f;
+	 
+	// Use this for initialization
+	void Start () {
+
+		Destroy (gameObject, destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Spawn/Spawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public Transform[] SpawnPoints;
+	public float spawnTime = 1.5f;
+	//public GameObject shapes;
+
+	 public GameObject[] shapes;
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("SpawnShapes",spawnTime,spawnTime); // Repeats the function/call
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void SpawnShape()
+	{
+		int spawnIndex = Random.Range (0,SpawnPoints.Length); //Sets the index number of the array randomly
+
+		int objectIndex = Random.Range (0,SpawnPoints.Length);  //Sets the object index of the array randomly
+
+		Instantiate(shapes[1],SpawnPoints[objectIndex].position,SpawnPoints [spawnIndex].rotation); //clones these objects by the spawn points
+	}
+}
